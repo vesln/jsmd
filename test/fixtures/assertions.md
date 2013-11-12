580a275cf357a94f623685fc0a3d8bafe8e650cd
Veselin Todorov <veselin@veselin.bg>
2013-11-12 18:33:08 +0200

    Add more tests

---
+++ test/fixtures/assertions.md
@@ -0,0 +1,17 @@
+```js
+var obj = { foo: 'bar' };
+var num = 1;
+var arr = [ 1, 2, 3];
+var regex = /foo/;
+var undef = undefined;
+var nil = null;
+
+obj; // => { foo: 'bar' }
+num; // => 1
+arr; // => [ 1, 2, 3 ];
+regex; // => /foo/;
+undef; // => undefined
+nil; // => null
+
+Math.min(1, 2) // => 1
+```
---
+++ test/jsmd.test.js
@@ -0,0 +1,34 @@
+var verifyFile = require('..').verifyFile;
+var run = function run(file, fn) {
+  verifyFile(fixture(file), {}, fn);
+};
+
+describe('jsmd', function() {
+  it('ignores code different than javascript', function(done) {
+    run('ruby', function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('ignores line comments that are not assertions', function(done) {
+    run('line-comments', function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('can assert properly', function(done) {
+    run('assertions', function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('returns an error when the verification was not successful', function(done) {
+    run('bad', function(err) {
+      err.code.should.eq(8);
+      done();
+    });
+  });
+});
